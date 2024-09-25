660b5edd397e6adb6d19f2ffd89e07597869dcbc
witchbladedovi4 <pidzes@vk.com>
2024-09-25 15:00:45 +0300

    Add files via upload

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -5,31 +5,72 @@ int main()
 
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
-	int sum_all = 0;
-	int sum_chet = 0;
-	int sum_nechet = 0;
-	int num;
+	int rub{};
+	int commission = 5;
 	while (true)
 	{
-		std::cout << "Введите число: \n";
-		std::cin >> num;
-		if (num == 0)
+		std::cout << "Сколько у вас рублей ? \n";
+		std::cin >> rub;
+		int choice{};
+		double commission_rub = commission * 100 % rub;
+		double rub_s_com = rub - commission_rub;
+
+		std::cout << "\n\n\n\nКонветор валют : \n";
+		std::cout << "1)Доллар (92р) \n";
+		std::cout << "2)Евро (103р) \n";
+		std::cout << "3)Юань (13р) \n";
+		std::cout << "4)Фарит (37р) \n";
+		std::cout << "5)Йена (0,6р) \n";
+		std::cin >> choice;
+		if (choice == 1)
+		{
+			int usd = 92;
+			double trade_usd = rub_s_com / usd;
+			std::cout << "Вы обменяли " << rub << "р На " << trade_usd << "$ С коммисией 5%, которая составила " << commission_rub << "р\n";
+		}
+		else if (choice == 2)
+		{
+			int eur = 103;
+			double trade_eur = rub_s_com / eur;
+			std::cout << "Вы обменяли " << rub << "р На " << trade_eur << "евро С коммисией 5%, которая составила " << commission_rub << "р\n";
+		}
+		else if (choice == 3)
+		{
+			int cny = 13;
+			double trade_cny = rub_s_com / cny;
+			std::cout << "Вы обменяли " << rub << "р На " << trade_cny << "юань С коммисией 5%, которая составила " << commission_rub << "р\n";
+		}
+		else if (choice == 4)
+		{
+			int farit = 37;
+			double trade_farit = rub_s_com / farit;
+			std::cout << "Вы обменяли " << rub << "р На " << trade_farit << "фаритов С коммисией 5%, которая составила " << commission_rub << "р\n";
+		}
+		else if (choice == 5)
+		{
+			double jpy = 0.6;
+			double trade_jpy = rub_s_com / jpy;
+			std::cout << "Вы обменяли " << rub << "р На " << trade_jpy << "йен С коммисией 5%, которая составила " << commission_rub << "р\n";
+		}
+		int end_choise{};
+		std::cout << "Еще раз ?\n(1)ДА\n(2)НЕТ\n";
+		std::cin >> end_choise;
+		if (end_choise == 1)
+		{
+			continue;
+		}
+		else if (end_choise == 2)
 		{
 			break;
 		}
-		else if (num % 2 == 0)
+		else
 		{
-			sum_chet += num;
+			std::cout << "Невырный выбор!";
+			break;
 		}
-		else if (num % 2 != 0)
-		{
-			sum_nechet += num;
-		}
-		sum_all += num;
 	}
-	std::cout << "Четные - " << sum_chet << "\n";
-	std::cout << "Нечетные - " << sum_nechet << "\n";
-	std::cout << "Сумма всех - " << sum_all << "\n";
+
+
 
 	return 0;
 }
