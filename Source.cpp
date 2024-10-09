0d9a0bc9bbd8377154b82d95035bf051c4c01a44
witchbladedovi4 <pidzes@vk.com>
2024-10-09 13:17:42 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,68 +1,60 @@
 #include <iostream>
 #include <Windows.h>
-#include <cstdlib>
-int main()
+
+
+int RetunrnSum(int a , int b)
+{
+	return a + b;
+}
+
+int RetuntRaz(int a, int b)
 {
 
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	srand(time(NULL));
-	int const SIZE = 10;
-	int arr1[SIZE]{}, arr2[SIZE]{};
-	int a{}, k{};
-	for (size_t i = 0; i < SIZE; i++)
-	{
-		arr1[i] = std::rand() % 11;
-	}
-	for (size_t i = 0; i < SIZE; i++)
-	{
-		std::cout << "Введите число от 1 до 10 " << "\n";
-		std::cin >> a;
-		if (a >= 0 && a <= 10) 
-		{
-			arr2[i] = a;
-		}
-		else
-		{
-			std::cout << "Анлаки броу";
-			return 0;
-		}
-		
-	}
-	for (size_t i = 0; i < 10; i++)
-	{
-		
-		if (arr1[i] == arr2[i])
-		{
-			k += 1;
-		}
-	}
-	std::cout << "рандомный массив\n";
+	return a - b;
+}
 
+int ReturnUmn(int a, int b)
+{
+	return a * b;
+}
 
-	for (size_t i = 0; i < 10; i++)
+int ReturnDel(int a, int b)
+{
+	if (b == 0)
 	{
-		std::cout << arr1[i] << " ";
-	}
-
-
-	std::cout << "\n\n\n\n\n";
-	std::cout << "Ваш массив\n";
-
-
-	for (size_t i = 0; i < 10; i++)
-	{
-		std::cout << arr2[i] << " ";
-	}
-	if (k > 0)
-	{
-		std::cout << "\nЛучший Й";
-		std::cout << std::string(k, 'О');
-		std::cout << "У\nУгадал " << k << " раз";
+		std::cout << "NA 0 NE DELITTYA";
+		return 0;
 	}
 	else
 	{
-		std::cout << "\nВидимо наводчик кантужен и ноль попаданий";
+		return a / b;
 	}
+	
+
+}
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char choise{};
+	int a{};
+	int b{};
+	std::cout << "Введите 1 число\n";
+	std::cin >> a;
+	std::cout << "Выберите действие + - * /\n";
+	std::cin >> choise;
+	std::cout << "Введите 2 число\n"; 
+	std::cin >> b;
+	if (choise == '+')
+		std::cout << RetunrnSum(a, b);
+	if (choise == '-')
+		std::cout << RetuntRaz(a, b);
+	if (choise == '*')
+		std::cout << ReturnUmn(a, b);
+	if (choise == '/')
+		std::cout << ReturnDel(a, b);
+	
 	return 0;
 }
