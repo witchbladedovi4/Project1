4a4ee211cf8ff8a5d3741443d2b2c8e50aa0d66e
witchbladedovi4 <pidzes@vk.com>
2024-10-16 13:13:14 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -3,59 +3,50 @@
 #include <cstdlib>
 
 
-void SetArr(int arr[], int size)
-{
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = rand() % 27 - 8;
-	}
-}
- void PrintArr(int arr[], int size)
- {
-	 for (int i = 0; i < size; i++)
-	 {
-		 std::cout << arr[i] << " ";
-	 }
-	 std::cout << "\n";
- }
- void MaxArr(int arr[], int size)
- {
-	 int max = -123;
-	 int i_max{};
-	 for (int i = 0; i < size; i++)
-	 {
-		 if (arr[i] > max) 
-		 {
-			 max = arr[i];
-			 i_max = i+1;
-		 } 
-	 }
-	 std::cout << "Max = " << max << " Pod nomerom " << i_max << "\n";
- }
- void MinArr(int arr[], int size)
- {
-	 int min = 123;
-	 int i_min{};
-	 for (int i = 0; i < size; i++)
-	 {
-		 if (arr[i] < min) 
-		 {
-			 min = arr[i];
-			 i_min = i+1;
-		 }
-	 }
-	 std::cout << "Min = " << min << " Pod nomerom " << i_min << "\n";
- }
 
-int main()
-{	
-	SetConsoleCP(1251);
-	SetConsoleOutputCP(1251);
-	const int size = 20;
-	int arr[size];
-	SetArr(arr, size);
-	PrintArr(arr, size);
-	MaxArr(arr, size);
-	MinArr(arr, size);
-	return 0;
+// Перегруженная функция для вывода массива int
+void Arr(int* arr, int size) 
+{
+    std::cout << "Массив int: ";
+    for (int i = 0; i < size; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+void Arr(float* arr, int size)
+{
+    std::cout << "Массив float: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+void Arr(char* arr, int size)
+{
+    std::cout << "Массив char: ";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int intArray[5] = { 1, 2, 3, 4, 5 };
+    float floatArray[5] = { 1.2f, 5.2f, 1.6f, 6.2f, 8.3f };
+    char charArray[5] = { 'a', 's', '2', 'x', 'd'};
+
+    Arr(intArray, 5);
+    Arr(floatArray, 5);
+    Arr(charArray, 5);
+
+    return 0;
 }
